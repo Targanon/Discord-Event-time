9935e864287bdf604f597295d02ad162f90e35ff
Targs <Targs@TARGSGAMER>
2026-05-05 14:02:09 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Discord_Event_time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Designer variables.
+        /// </summary>
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.CheckBox chkFull;
+        private System.Windows.Forms.CheckBox chkRelative;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer statusTimer;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.chkFull = new System.Windows.Forms.CheckBox();
+            this.chkRelative = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.AllowDrop = true;
+            this.dateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(4, 13);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(326, 26);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(6, 18);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(174, 44);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate NEW Code";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(18, 135);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(327, 26);
+            this.outputBox.TabIndex = 2;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyButton.Location = new System.Drawing.Point(6, 14);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(174, 63);
+            this.copyButton.TabIndex = 7;
+            this.copyButton.Text = "Copy to Clipbaord";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // chkFull
+            // 
+            this.chkFull.AutoSize = true;
+            this.chkFull.Checked = true;
+            this.chkFull.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFull.Location = new System.Drawing.Point(6, 15);
+            this.chkFull.Name = "chkFull";
+            this.chkFull.Size = new System.Drawing.Size(119, 20);
+            this.chkFull.TabIndex = 4;
+            this.chkFull.Text = "Full Date format";
+            this.chkFull.UseVisualStyleBackColor = true;
+            this.chkFull.CheckedChanged += new System.EventHandler(this.chkFull_CheckedChanged);
+            // 
+            // chkRelative
+            // 
+            this.chkRelative.AutoSize = true;
+            this.chkRelative.Checked = true;
+            this.chkRelative.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkRelative.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkRelative.Location = new System.Drawing.Point(6, 40);
+            this.chkRelative.Name = "chkRelative";
+            this.chkRelative.Size = new System.Drawing.Size(107, 20);
+            this.chkRelative.TabIndex = 5;
+            this.chkRelative.Text = "<< Thats in >>";
+            this.chkRelative.UseVisualStyleBackColor = true;
+            this.chkRelative.CheckedChanged += new System.EventHandler(this.chkRelative_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "By Targs35";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ver";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 265);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(359, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // statusTimer
+            // 
+            this.statusTimer.Interval = 3000;
+            this.statusTimer.Tick += new System.EventHandler(this.statusTimer_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkRelative);
+            this.groupBox1.Controls.Add(this.chkFull);
+            this.groupBox1.Location = new System.Drawing.Point(14, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 75);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.generateButton);
+            this.groupBox2.Location = new System.Drawing.Point(159, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(186, 75);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step 3";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dateTimePicker);
+            this.groupBox3.Location = new System.Drawing.Point(12, 9);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(333, 48);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Step 1";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.copyButton);
+            this.groupBox4.Location = new System.Drawing.Point(159, 167);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(186, 86);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Step 4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(359, 287);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Discord Event Generator";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Event_time
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _settingsPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Discord Event time",
+            "window.settings"
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+
+            // Set the clock icon
+            try
+            {
+                this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            }
+            catch
+            {
+                // If loading fails, use default
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            if (version != null)
+            {
+                label2.Text = $"{label2.Text} v{version.Major}.{version.Minor}.{version.Build}";
+            }
+            LoadWindowPosition();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveWindowPosition();
+        }
+
+        private void LoadWindowPosition()
+        {
+            try
+            {
+                if (!File.Exists(_settingsPath))
+                    return;
+
+                var lines = File.ReadAllLines(_settingsPath);
+                if (lines.Length < 5)
+                    return;
+
+                if (!int.TryParse(lines[0], out var x)) return;
+                if (!int.TryParse(lines[1], out var y)) return;
+                if (!int.TryParse(lines[2], out var w)) return;
+                if (!int.TryParse(lines[3], out var h)) return;
+                var state = lines[4];
+
+                var rect = new Rectangle(x, y, w, h);
+
+                // Validate that at least part of the window will be on a screen
+                bool visible = Screen.AllScreens.Any(s => s.WorkingArea.IntersectsWith(rect));
+                if (!visible)
+                    return;
+
+                this.StartPosition = FormStartPosition.Manual;
+                this.Bounds = rect;
+
+                if (string.Equals(state, "Maximized", StringComparison.OrdinalIgnoreCase))
+                    this.WindowState = FormWindowState.Maximized;
+                else
+                    this.WindowState = FormWindowState.Normal;
+            }
+            catch
+            {
+                // Ignore and use default position
+            }
+        }
+
+        private void SaveWindowPosition()
+        {
+            try
+            {
+                var dir = Path.GetDirectoryName(_settingsPath);
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+
+                var bounds = this.WindowState == FormWindowState.Normal ? this.Bounds : this.RestoreBounds;
+                var state = this.WindowState == FormWindowState.Maximized ? "Maximized" : "Normal";
+
+                File.WriteAllLines(_settingsPath, new[]
+                {
+                    bounds.X.ToString(),
+                    bounds.Y.ToString(),
+                    bounds.Width.ToString(),
+                    bounds.Height.ToString(),
+                    state
+                });
+            }
+            catch
+            {
+                // Ignore failures to save settings
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime localDateTime = dateTimePicker.Value;
+                DateTime utcDateTime = localDateTime.ToUniversalTime();
+                long unixTimestamp = ((DateTimeOffset)utcDateTime).ToUnixTimeSeconds();
+
+                string timeFormat = chkFull.Checked ? "f" : "t";  // :f for full, :t for short time
+                string formatted = $"<t:{unixTimestamp}:{timeFormat}>";
+                string relative = $"<t:{unixTimestamp}:R>";
+
+                var parts = new System.Collections.Generic.List<string>();
+                parts.Add(formatted);
+                if (chkRelative.Checked) parts.Add(relative);
+                outputBox.Text = string.Join(" ", parts);
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    toolStripStatusLabel1.Text = $"Error: {ex.Message}";
+                    statusTimer.Start();
+                }
+                catch
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(outputBox.Text))
+                return;
+
+            try
+            {
+                Clipboard.SetText(outputBox.Text);
+                toolStripStatusLabel1.Text = "Copied to clipboard!";
+                statusTimer.Start();
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    toolStripStatusLabel1.Text = $"Copy failed: {ex.Message}";
+                    statusTimer.Start();
+                }
+                catch
+                {
+                    MessageBox.Show($"Copy failed: {ex.Message}");
+                }
+            }
+        }
+
+        private void statusTimer_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = string.Empty;
+            statusTimer.Stop();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            if (version != null)
+            {
+                label2.Text = $"{label2.Text} v{version.Major}.{version.Minor}.{version.Build}";
+            }
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkFull_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkRelative_CheckedChanged(object sender, EventArgs e)
+        {
+            // Regenerate the output when checkbox is toggled
+            generateButton_Click(null, null);
+        }
+    }
+}
